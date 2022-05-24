78a48b9e4ecf39285c8d14c2bf92cd5914eb1e74
Aurelio Malheiros <aureliomalheiros@gmail.com>
2022-05-24 16:24:43 -0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ sudo -i apt install -y strongswan
 
 sudo bash -c "echo \#Static Routes  >> /etc/network/interfaces"
 sudo bash -c "echo up route del -net 0.0.0.0/0 gw `ip route | grep default |egrep '[0-9\.]{6,}[$1]' | awk  {'print $3'}` dev eth0  >> /etc/network/interfaces"
-sudo bash -c "echo up route del -net 0.0.0.0/0 gw `ip addr | egrep '[0-9\.]{6,}/24' | awk '{print $2}' | cut -d/ -f1` dev eth0  >> /etc/network/interfaces"
+sudo bash -c "echo up route add -net 0.0.0.0/0 gw `ip addr | egrep '[0-9\.]{6,}/24' | awk '{print $2}' | cut -d/ -f1` dev eth0  >> /etc/network/interfaces"
 
 tab="$(printf '\t')"
 sudo bash -c "cat << EOF > /etc/ipsec.conf
